aa17fac0083ec58312b527daa5e872282e7edb8f
Nomad29 <alexandramelancon@outlook.com>
2020-04-06 02:03:37 -0400

    Animated some enemies + Venom shots interval/power + Updated the venom shot button in-game and beginning
    .

--- prototype1/Assets/Shoot.cs
+++ prototype1/Assets/Shoot.cs
@@ -1,4 +1,4 @@
-﻿/*****************
+﻿ /*****************
 
 How to shoot bullets
 by Bracer Jack on YouTube
@@ -27,7 +27,7 @@ public class Shoot : MonoBehaviour
     public float Bullet_Forward_Force;
 
     // Number of bullets to shoot
-    public int numberOfObjects = 7;
+    public int numberOfObjects = 1;
 
     // Use this for initialization
     void Start()
@@ -42,30 +42,42 @@ public class Shoot : MonoBehaviour
         for (int i = 0; i < numberOfObjects; i++)
         {
 
-            // If left mouse click is pushed.
+            // If SPACE or ENTER bar key is pressed.
             // The Down does not let the player using continuously the shoot mouse click
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
             {
-                // The Bullet instantiation happens here.
-                GameObject Temporary_Bullet_Handler;
-                Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
-                Debug.Log("Bullet shooted");
-
-                // Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
-                Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
-
-                // Retrieve the Rigidbody component from the instantiated Bullet and control it. 
-                // Important that the things you want to fire upon with the bullets to possess a RigidBody also.
-                Rigidbody Temporary_RigidBody;
-                Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
-
-                // Tell the bullet to be "pushed" forward by an amount set by Bullet_Forward_Force.
-                Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
-
-                // Basic Clean Up, set the Bullets to self destruct after 0.3 Seconds. Can be bigger number if used as a projectile and not a boost.
-                Destroy(Temporary_Bullet_Handler, 0.3f);
+                // Start a delay function of 0.5 second
+                StartCoroutine(Reuse());
             }
         }
+    }
 
+    public IEnumerator Reuse()
+    {
+        // The Bullet instantiation happens here.
+        GameObject Temporary_Bullet_Handler;
+        Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+        Debug.Log("Bullet shooted");
+
+        // Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
+        Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+
+        // Retrieve the Rigidbody component from the instantiated Bullet and control it. 
+        // Important that the things you want to fire upon with the bullets to possess a RigidBody also.
+        Rigidbody Temporary_RigidBody;
+        Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+
+        // Tell the bullet to be "pushed" forward by an amount set by Bullet_Forward_Force.
+        Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
+
+        // Basic Clean Up, set the Bullets to self destruct after 0.3 Seconds. Can be bigger number if used as a projectile and not a boost.
+        Destroy(Temporary_Bullet_Handler, 0.3f);
+
+        numberOfObjects = 0;
+
+        // Start a delay of 0.5 second
+        yield return new WaitForSeconds(0.5f);
+
+        numberOfObjects = 7;
     }
 }
